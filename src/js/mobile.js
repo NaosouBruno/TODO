97cfceeb4aa6e39f2ce01d2d53662cbd91ec3c6f
Bruno Bartolomeu <brunoowap@gmail.com>
2022-05-05 21:07:39 -0300

    adicionando primeios elementos da versao mobile

---
+++ src/js/mobile.js
@@ -0,0 +1,11 @@
+function openNav() {
+  document.querySelector(".item-list").style.width = "250px";
+  document.querySelector(".menuBar").style.marginLeft = "250px";
+
+  
+}
+
+/* function closeNav() {
+  document.querySelector("item-list").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+} */
