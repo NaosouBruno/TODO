26a70fdc7c3c0c0c4c05eb23f01e9642b487e5cd
Bruno Bartolomeu <brunoowap@gmail.com>
2022-05-06 12:00:00 -0300

    alterando alguns elementos css do mobile

--- src/js/mobile.js
+++ src/js/mobile.js
@@ -1,8 +1,9 @@
 function openNav() {
   document.querySelector(".item-list").style.width = "250px";
+document.querySelector(".item-list").style.display = "block";
+/*     document.querySelector(".teste").style.display = "block"; */
   document.querySelector(".menuBar").style.marginLeft = "250px";
 
-  
 }
 
 /* function closeNav() {
